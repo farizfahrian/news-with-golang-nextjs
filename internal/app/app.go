11c9a698b69104f44615721308e66db9713ee382
Snawlz Zr <farizfahrian53@gmail.com>
2025-05-17 10:12:45 +0800

    add swagger docs

--- internal/app/app.go
+++ internal/app/app.go
@@ -17,6 +17,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gofiber/contrib/swagger"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -76,6 +77,16 @@ func RunServer() {
 	app.Use(logger.New(logger.Config{
 		Format: "${time} ${ip} ${status} - ${latency} ${method} ${path}\n",
 	}))
+
+	if os.Getenv("ENV") != "production" {
+		cfg := swagger.Config{
+			BasePath: "/api",
+			FilePath: "./docs/swagger.json",
+			Path:     "docs",
+			Title:    "News API",
+		}
+		app.Use(swagger.New(cfg))
+	}
 
 	api := app.Group("/api")
 	api.Post("/login", authHandler.Login)
